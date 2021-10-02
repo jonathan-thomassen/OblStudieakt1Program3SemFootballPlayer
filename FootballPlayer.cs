a2dc1dff246a3af038e049ab8b7ac65dc6e0a15b
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-10-02 14:36:25 +0200

    Add project files.

---
+++ FootballPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OblStudieakt1Program3Sem
+{
+    public class FootballPlayer
+    {
+        private double _price;
+        private string _name;
+        private int _id;
+        private int _shirtNumber;
+        private static int _nextId = 1;
+
+        public int Id
+        {
+            get => _id;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length >= 4)
+                    _name = value;
+                else
+                    throw new ArgumentException();
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value > 0.0)
+                    _price = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value >= 1 && value <= 100)
+                    _shirtNumber = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public FootballPlayer()
+        {
+            _id = _nextId;
+            _nextId++;
+        }
+    }
+}
